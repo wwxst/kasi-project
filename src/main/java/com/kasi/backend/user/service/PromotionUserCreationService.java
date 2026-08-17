08c08b0e15b7cb9d3eecbadb3d4e419a0a75c476
wwxst <19193171667@163.com>
2026-08-17 09:16:39 +0800

    feat: centralize promotion user creation

---
+++ src/main/java/com/kasi/backend/user/service/PromotionUserCreationService.java
@@ -0,0 +1,8 @@
+package com.kasi.backend.user.service;
+
+import com.kasi.backend.user.entity.PromotionUser;
+
+public interface PromotionUserCreationService {
+
+    void create(PromotionUser user);
+}
